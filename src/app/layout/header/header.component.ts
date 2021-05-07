c00b086ec522382ba287c65814377237b2c8d692
ilyas ahmed <ilyasahmed081@gmail.com>
2021-05-07 17:01:03 +0500

    header added

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  toggleButton: boolean = false;
+  sidebar: boolean = false;
+  fadeSection: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleMenu() {
+    // animating icon
+    this.toggleButton = !this.toggleButton;
+
+    // animating sidebar
+    this.sidebar = !this.sidebar;
+
+    // enabling fade section
+    this.fadeSection = !this.fadeSection;
   }
 
 }
