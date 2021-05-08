d92bddc22256a2c47a3f563b626dabfea09de859
muhammadidreesshaikh <idreesiqbal104@gmail.com>
2021-05-09 01:47:50 +0500

    homepage in working

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepageComponent implements OnInit {
 
+  data: any = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.data = [
+      {
+        title: 'Community',
+        service: 'Service 1'
+      },
+      {
+        title: 'Housing',
+        service: 'Service 1'
+      },
+      {
+        title: 'Jobs',
+        service: 'Service 1'
+      },
+      {
+        title: 'Services',
+        service: 'Service 1'
+      },
+      {
+        title: 'For Sale',
+        service: 'Service 1'
+      }
+    ]
   }
 
 }
