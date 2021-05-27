7331d145c42d31bac6d3fbbeca2d4090176d884d
ilyas ahmed <ilyasahmed081@gmail.com>
2021-05-28 00:53:22 +0500

    fixes

--- src/app/components/posting/view-postings/view-postings.component.ts
+++ src/app/components/posting/view-postings/view-postings.component.ts
@@ -15,42 +15,42 @@ export class ViewPostingsComponent implements OnInit {
     this.data = [
       {
         id: 1,
-        image: '../../../../assets/img/add4.jpg',
+        image: 'assets/img/add4.jpg',
         name: 'Drum Tracks',
         date: 'March, 5, 2020 -',
         location: 'Berlin'
       },
       {
         id: 2,
-        image: '../../../../assets/img/add2.jpg',
+        image: 'assets/img/add2.jpg',
         name: 'Seeking Piano',
         date: 'April, 20, 2021 -',
         location: 'Remote'
       },
       {
         id: 3,
-        image: '../../../../assets/img/add1.jpg',
+        image: 'assets/img/add1.jpg',
         name: 'Serious Dark',
         date: 'May, 9, 2021 -',
         location: 'Berlin'
       },
       {
         id: 4,
-        image: '../../../../assets/img/add5.jpg',
+        image: 'assets/img/add5.jpg',
         name: 'Music Room',
         date: 'June, 22, 2020 -',
         location: 'Hamburg'
       },
       {
         id: 5,
-        image: '../../../../assets/img/add3.jpg',
+        image: 'assets/img/add3.jpg',
         name: '3D Groovy',
         date: 'April, 28, 2021 -',
         location: 'Berlin'
       },
       {
         id: 6,
-        image: '../../../../assets/img/add6.jpg',
+        image: 'assets/img/add6.jpg',
         name: ' Seeking piano',
         date: 'April, 20, 2021 -',
         location: 'Berlin'
@@ -61,42 +61,42 @@ export class ViewPostingsComponent implements OnInit {
     this.card = [
       {
         id: 1,
-        image: '../../../../assets/img/add1.jpg',
+        image: 'assets/img/add1.jpg',
         name: 'Serious Dark',
         date: 'May, 9, 2021 -',
         location: 'Berlin'
       },
       {
         id: 2,
-        image: '../../../../assets/img/add2.jpg',
+        image: 'assets/img/add2.jpg',
         name: 'Seeking Piano',
         date: 'April, 20, 2021 -',
         location: 'Remote'
       },
       {
         id: 3,
-        image: '../../../../assets/img/add3.jpg',
+        image: 'assets/img/add3.jpg',
         name: '3D Groovy',
         date: 'April, 28, 2021 -',
         location: 'Berlin'
       },
       {
         id: 4,
-        image: '../../../../assets/img/add4.jpg',
+        image: 'assets/img/add4.jpg',
         name: 'Drum Tracks',
         date: 'March, 5, 2020 -',
         location: 'Berlin'
       },
       {
         id: 5,
-        image: '../../../../assets/img/add5.jpg',
+        image: 'assets/img/add5.jpg',
         name: 'Music Room',
         date: 'June, 22, 2020 -',
         location: 'Hamburg'
       },
       {
         id: 6,
-        image: '../../../../assets/img/add6.jpg',
+        image: 'assets/img/add6.jpg',
         name: ' Seeking piano',
         date: 'April, 20, 2021 -',
         location: 'Berlin'
