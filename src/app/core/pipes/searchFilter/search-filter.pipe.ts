1dfd41c3b55f130a3c3dd61aa5b0fa78900cb37c
Muhammad Awais <awaisshaikh94@gmail.com>
2021-06-22 19:00:37 +0500

    added custom filter pipe

---
+++ src/app/core/pipes/searchFilter/search-filter.pipe.ts
@@ -0,0 +1,29 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'searchFilter'
+})
+export class SearchFilterPipe implements PipeTransform {
+
+  /**
+   * Transform
+   *
+   * @param {any[]} items
+   * @param {string} searchText
+   * @returns {any[]}
+   */
+   transform(items: any[], searchText: string): any[] {
+    if (!items) {
+      return [];
+    }
+    if (!searchText) {
+      return items;
+    }
+    searchText = searchText.toLocaleLowerCase();
+
+    return items.filter(it => {
+      return it.toLocaleLowerCase().includes(searchText);
+    });
+  }
+
+}
