c0d22e03be26f2c113dc068b0101f4317851828e
Muhammad Awais <awaisshaikh94@gmail.com>
2021-06-22 19:00:11 +0500

    added custom filter pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
+import { SearchFilterPipe } from './core/pipes/searchFilter/search-filter.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    SearchFilterPipe
   ],
   imports: [
     BrowserModule,
