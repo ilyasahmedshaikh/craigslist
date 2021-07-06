e14ef3fc4917d77dc7304bf78c148c793c56c06e
Muhammad Awais <awaisshaikh94@gmail.com>
2021-07-06 15:52:41 +0500

    signup capability added

--- src/app/components/auth/create-account/create-account.component.ts
+++ src/app/components/auth/create-account/create-account.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { ConfigService } from '../../../core/http/config/config.service';
+import { ApiService } from '../../../core/http/api/api.service';
 
 @Component({
   selector: 'app-create-account',
@@ -11,7 +15,11 @@ export class CreateAccountComponent implements OnInit {
   programForm: any = FormGroup;
 
   constructor(
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private router: Router,
+    private auth: AngularFireAuth,
+    private config: ConfigService,
+    private apiCallService: ApiService
   ) { }
 
   ngOnInit(): void {
@@ -26,6 +34,32 @@ export class CreateAccountComponent implements OnInit {
       password: ['', Validators.required],
       confirmPassword: ['', Validators.required],
     });
+  }
+
+  signup() {
+    // making authentic account for detailer in Database
+    this.auth.createUserWithEmailAndPassword(this.programForm.value.email, this.programForm.value.password)
+      .then(value => {
+        // adding detailer to firestore for user_type and other profile biodata
+        let data = {
+          firstname: this.programForm.value.firstname,
+          lastname: this.programForm.value.lastname,
+          email: this.programForm.value.email,
+          user_type: 'user'
+        };
+
+        let request = this.apiCallService.post(this.config.collections.users, data);
+        request.then(() => {
+          this.router.navigateByUrl("/auth/login");
+        })
+        .catch((error) => {
+          alert(error);
+        });
+      })
+      .catch(err => {
+        alert(err.message);
+        console.log('Something went wrong: ', err.message);
+      });
   }
 
 }
