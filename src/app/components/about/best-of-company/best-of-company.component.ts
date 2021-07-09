3b1e8efd380f3876ff00a9d9d576fa1b025a5639
Muhammad Awais <awaisshaikh94@gmail.com>
2021-07-09 23:10:06 +0500

    in Photo6 This complete site does not work as such as the search bar and the postings

--- src/app/components/about/best-of-company/best-of-company.component.ts
+++ src/app/components/about/best-of-company/best-of-company.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ConfigService } from '../../../core/http/config/config.service'
+import { ApiService } from '../../../core/http/api/api.service';
 
 @Component({
   selector: 'app-best-of-company',
@@ -7,9 +10,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BestOfCompanyComponent implements OnInit {
 
-  constructor() { }
+  data: any = [];
+  categories: any = [];
+
+  searchText: string = '';
+
+  constructor(
+    private router: Router,
+    private config: ConfigService,
+    private api: ApiService,
+  ) { }
 
   ngOnInit(): void {
+    this.getPostings();
+  }
+
+  getPostings() {
+    this.api.getAll(this.config.collections.posts).subscribe(res => {
+      this.data = res;
+      this.getCategories();
+      console.log(this.data);
+    })
+  }
+
+  getCategories() {
+    this.api.getAll(this.config.collections.categories).subscribe(res => {
+      this.categories = res;
+      console.log(this.categories);
+    })
+  }
+
+  findCategory(item) {
+    let category = this.categories.find(i => i.id == item.categoryId);
+    return category.name;
+  }
+
+  viewDetailed(item) {
+    this.router.navigate(['/posting/posting-detail'], { state: { data: item, posting: {} } });
+  }
+
+  getSearchQuery(event) {
+    this.searchText = event;
   }
 
 }
